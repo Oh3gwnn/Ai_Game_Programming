24fbca37e676b69d383a5cb015429b2c64a013c5
hyoungteak <aqko0719@gmail.com>
2021-07-20 16:17:33 +0900

    README
    README File update

---
+++ AI_Chapter_3-4/AI_Chapter_3-4_Script/Changhee/Go4.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Go4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("portal"))
+            SceneManager.LoadScene("Ai_2 jh");
+
+        else
+        {
+            SceneManager.LoadScene("test1");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Unity_AI_Game_Programming
+	유니티 AI 프로그래밍 학습 동아리 깃허브입니다.
+	변경사항 README 파일 참고
+
+## 1. AI_Project_4_Script
+	3. 프로덕션 시스템
+	4. 배경과 AI
+	배경과 오브젝트에 상호작용하는 AI를 중점으로 학습
+
+**2021-07-20**
+
+* 실전 게임 기초 AI 프로그래밍 교재 Chapter 3, 4 기반
+
+* 유니티 에셋 스토어의 **'Pixel Art Top Down - Basic'** 이용
+
+* 개별 스크립트 업로드
+
+* Chapter 3, 4 PPT, PDF 제작
