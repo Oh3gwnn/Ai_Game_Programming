1a7eedb089ada7721ffc845493be39b31740b3ad
hyoungteak <aqko0719@gmail.com>
2021-08-01 22:07:18 +0900

    Add Chapter 2

---
+++ AI_Chapter_2/AI_Chapter_2_Script/CharacterAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAttribute : MonoBehaviour
+{
+    public float Radius;
+    void Start()
+    {}
+    
+    void Update()
+    {}
+}
---
+++ AI_Chapter_2/AI_Chapter_2_Script/EnemyBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    public GameObject Target;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Vector3 dir = Target.transform.position - transform.position;
+            dir.Normalize();
+
+            float distance = Vector3.Distance(Target.transform.position, transform.position);
+            CharacterAttribute targetAttribute = Target.GetComponent<CharacterAttribute>();
+            CharacterAttribute myAttribute = gameObject.GetComponent<CharacterAttribute>();
+            if (distance > targetAttribute.Radius + myAttribute.Radius)
+            {
+                transform.position += dir * moveSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+    }
+
+    void Start()
+    {    
+    }
+    
+    void Update()
+    {
+        
+    }
+}
